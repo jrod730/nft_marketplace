1eabaf2bdd462bb3a4c8ecf3d4320b4dc0042bba
Joseph Rodriguez <jrodr100@neiu.edu>
2022-07-25 10:30:51 -0500

    add components and restructure app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+import { Navbar, Footer } from '../components/index';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/b5083fa894.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+
+);
 
 export default MyApp;
